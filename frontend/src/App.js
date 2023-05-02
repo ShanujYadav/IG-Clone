44995c64b9985bf8e5cebf3d554511c7dc9b9fd6
Shanuj Yadav <92615408+ShanujYadav@users.noreply.github.com>
2023-05-02 17:40:02 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import CreatePost from './components/CreatePost';
+import Home from './components/Home';
+function App() {
+  return (<>
+  <Home/>
+  </>
+  );
+}
+
+export default App;
